f9b52b88b85a51b8ac2a99cffb8ca5f572946056
kellynichm <kellynichm@gmail.com>
2023-12-05 12:22:00 -0800

    Putting function in line

--- ModifiedStreamlit.py
+++ ModifiedStreamlit.py
@@ -160,12 +160,12 @@ with st.container():
     if uploaded_file:
         st.markdown("<h3 style='text-align: center; color: red;'>Generator Model Response</h3>", unsafe_allow_html=True)
         with st.spinner('Custom PaLM model is working to generate, wait.....'):
-            #doc = fitz.open("pdf",uploaded_file)
-            #text =""
-            #for page_num in range (doc.page_count):
-            #    page = doc[page_num]
-            #    text += page.get_text()
-            text = extract_text_from_pdf(uploaded_file)
+            doc = fitz.open("pdf",uploaded_file)
+            text =""
+            for page_num in range (doc.page_count):
+                page = doc[page_num]
+                text += page.get_text()
+            #text = extract_text_from_pdf(uploaded_file)
             #exception handling for length needed
             prompt = prompt_template.format(text=text)
             response = get_text_generation(prompt=prompt, temperature = st.session_state['temperature'],
